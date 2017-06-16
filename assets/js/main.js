a40629add7955b7015b26a7034ad80a80290fd47
Joselin Grez <joselingrez.m@hotmail.es>
2017-06-16 12:28:18 -0300

    Seccion collage

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,3 +24,18 @@ function validar() {
 
 validarPassword();
 }
+
+function drag(ev) {
+	ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function finalDrop(ev){
+	ev.preventDefault();
+}
+
+function drop(ev){
+	ev.preventDefault();
+
+	var dato = ev.dataTransfer.getData("text");
+	ev.target.appendChild(document.getElementById(dato));
+}
