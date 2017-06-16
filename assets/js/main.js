621cf27d7eef29f7cbcd04e446da51a7b5e870d9
Joselin Grez <joselingrez.m@hotmail.es>
2017-06-16 00:00:29 -0300

    Arreglando vista mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,19 +4,19 @@ function validar() {
 
 	function validarUsuario(){
 		if(usuario == "" || usuario.length == 0){ // valida que el campo usuario no este vacio			
-   	alert("Debes ingresar tu usuario");
-   	}else if(usuario.length < 6){
-   		alert("El nombre de usuario debe contener al menos 6 caracteres");
-   	}
-}
+			alert("Debes ingresar tu usuario");
+		}else if(usuario.length < 6){
+			alert("El nombre de usuario debe contener al menos 6 caracteres");
+		}
+	}
 
-validarUsuario();
+	validarUsuario();
 
-function validarPassword(){
+	function validarPassword(){
 	if(password == "" || password.length == 0){ // valida que el campo usuario no este vacio			
-   		alert("Debes ingresar tu contraseña");
-   	}else if(password.length < 6){
-   		alert("La contraseña debe contener al menos 6 caracteres");
+		alert("Debes ingresar tu contraseña");
+	}else if(password.length < 6){
+		alert("La contraseña debe contener al menos 6 caracteres");
 	}else if(password === "123456"){
 		alert("Error, tu contraseña no puede ser 123456, Debes ingresar una contraseña válida");
 	}
